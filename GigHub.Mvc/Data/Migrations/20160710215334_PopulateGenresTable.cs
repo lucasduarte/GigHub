034cee9cf01c9d20c1a255b914f5f0df06e38c17
Lucas Duarte <lucasduarte599@gmail.com>
2016-07-10 18:59:49 -0300

    Populates Genres table.

---
+++ GigHub.Mvc/Data/Migrations/20160710215334_PopulateGenresTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHub.Mvc.Data.Migrations
+{
+    public partial class PopulateGenresTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Jazz')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Blues')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Rock')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Country')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Genres WHERE Id IN (1,2,3,4)");
+        }
+    }
+}
